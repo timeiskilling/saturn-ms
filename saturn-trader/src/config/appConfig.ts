9715bb07959bbaea0c2025676e6d4ca1c4de2c23
bedolaga <zpavlenko054@gmail.com>
2026-05-26 02:52:02 +0300

    Update appConfig.ts

--- saturn-trader/src/config/appConfig.ts
+++ saturn-trader/src/config/appConfig.ts
@@ -5,19 +5,20 @@ export interface AppConfig {
   grpcBaseUrl: string;
   heliuspUrl: string;
 }
+
 export const appConfig: AppConfig = {
   priceServiceBaseUrl:
-    import.meta.env.VITE_PRICE_SERVICE_URL || "https://sutrn.com",
+    process.env.VITE_PRICE_SERVICE_URL || "https://sutrn.com",
 
   tokenListRefreshIntervalSec: Number(
-    import.meta.env.VITE_TOKEN_LIST_REFRESH_INTERVAL_SEC || 7320,
+    process.env.VITE_TOKEN_LIST_REFRESH_INTERVAL_SEC || 7320,
   ),
 
-  sessionBaseUrl: import.meta.env.VITE_SESSION_URL || "https://sutrn.com",
+  sessionBaseUrl: process.env.VITE_SESSION_URL || "https://sutrn.com",
 
-  grpcBaseUrl: import.meta.env.VITE_GRPC_URL || "https://sutrn.com",
+  grpcBaseUrl: process.env.VITE_GRPC_URL || "https://sutrn.com",
 
-  heliuspUrl: import.meta.env.VITE_HELIUS_API_KEY
-    ? `https://mainnet.helius-rpc.com/?api-key=${import.meta.env.VITE_HELIUS_API_KEY}`
+  heliuspUrl: process.env.VITE_HELIUS_API_KEY
+    ? `https://mainnet.helius-rpc.com/?api-key=${process.env.VITE_HELIUS_API_KEY}`
     : "https://api.devnet.solana.com",
 };
